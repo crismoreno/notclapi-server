381b0e4454b35dbec3771bd1e7bc8539a481a4a4
Cristina Moreno <moreno.medran@gmail.com>
2017-12-24 16:35:56 +0100

    seting heroku

---
+++ models/projects.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const projectSchema = new Schema({
+  name: String,
+  tags: [String],
+  img: String,
+  description: String,
+  kind: String,
+  date: String
+}, {
+  timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+});
+
+
+module.exports = mongoose.model('Project', projectSchema);
